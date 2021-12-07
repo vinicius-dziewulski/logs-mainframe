f5e0e9d8e511c894382a9de066d18ce1ac51e591
Dziewulski Vinicius <vinicius.dziewulski@volvo.com>
2021-12-07 09:45:14 -0300

    Changing API_PATH variable localtion

--- src/app/services/table-content.service.ts
+++ src/app/services/table-content.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, ComponentFactoryResolver } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import { API_PATH } from 'src/environments/environment';
+// import { API_PATH } from 'src/environments/environment';
 import { IResponse } from '../interfaces/ITableContent';
 import { QueryModel } from '../query.model';
 
@@ -12,6 +12,9 @@ export class TableContentService {
   constructor(private httpClient: HttpClient) { }
 
   sendQuery(query: QueryModel){
+
+    const API_PATH = "https://ebm-cta-test-datasquad-apim.azure-api.net/tbalogs-viewer/v2/searchDB/v2";
+    
     const headers = {
       'Content-Type': 'application/json',
       'Ocp-Apim-Subscription-Key': '87b917430d294c88a67008de74aa8e70',
